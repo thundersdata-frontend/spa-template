8a26f46f6853cba59390897d4f3bc4d413003616
aisriver <767882503@qq.com>
2019-11-08 11:49:27 +0800

    Update request.token.js

---
+++ request.token.js
@@ -0,0 +1,18 @@
+/**
+ * 通过同步方法
+ */
+// const getToken = () => '123';
+
+/**
+ * 通过异步方法
+ */
+// const getToken = async () => {
+//   const token = await new Promise(resolve =>
+//     setTimeout(() => {
+//       resolve('123');
+//     }, 1000),
+//   );
+//   return token;
+// };
+
+// module.exports = getToken;
