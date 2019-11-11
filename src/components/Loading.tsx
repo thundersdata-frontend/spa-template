272985d8941aa21c926d67aa10faba11052f97f7
chj_damon <chjdamon@gmail.com>
2019-11-11 17:05:13 +0800

    fix: 修复spinning永远在左上角的问题

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,5 +2,17 @@ import * as React from 'react';
 import { Spin } from 'antd';
 
 export default function Loading() {
-  return <Spin />;
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Spin />
+    </div>
+  );
 }
