387b789c066bcc11a1778dda9bd88136682452f0
chj_damon <chjdamon@gmail.com>
2019-11-18 16:12:30 +0800

    chore: 修改request.token的示例

--- request.token.js
+++ request.token.js
@@ -1,18 +1,8 @@
-/**
- * 通过同步方法
- */
-// const getToken = () => '123';
+// import lscache from 'lscache';
 
-/**
- * 通过异步方法
- */
-// const getToken = async () => {
-//   const token = await new Promise(resolve =>
-//     setTimeout(() => {
-//       resolve('123');
-//     }, 1000),
-//   );
-//   return token;
+// module.exports = {
+//   withCredentials: false,
+//   async requestToken() {
+//     return Promise.resolve(lscache.get('token'));
+//   },
 // };
-
-// module.exports = getToken;
