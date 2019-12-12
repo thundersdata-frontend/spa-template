e0a7bf00cbadb551fc0b90a76d808013ad83b6c0
hss <huangshanshan@thundersdata.com>
2019-12-12 17:47:34 +0800

    refactor: 增加company参数

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -4,7 +4,7 @@
  * @作者: 陈杰
  * @Date: 2019-10-23 21:12:06
  * @LastEditors: 黄姗姗
- * @LastEditTime: 2019-12-09 16:29:04
+ * @LastEditTime: 2019-12-12 15:55:27
  */
 
 export interface CustomWindow extends Window {
@@ -19,6 +19,7 @@ export interface CustomWindow extends Window {
     client_secret: string;
     password_min: number;
     password_max: number;
+    company: number,
   };
 }
 
