c7a2f200454bf180922770a04f89e206b032b1e1
liaojun <767882503@qq.com>
2020-06-29 16:51:20 +0800

    feat: 新增model内使用的request方法及用户信息存放位置提示

---
+++ src/hooks/useModelRequest.ts
@@ -0,0 +1,29 @@
+/*
+ * @文件描述: 针对models内容的请求是在初始化的时候统一进行的，所以封装了该方法
+ * 1、未登录情况不应该请求这些数据；
+ * 2、登录后判断有菜单内容才请求，通过缓存的token判断无效；
+ * @公司: thundersdata
+ * @作者: 廖军
+ * @Date: 2020-06-17 15:48:22
+ * @LastEditors: 廖军
+ * @LastEditTime: 2020-06-29 16:48:01
+ */
+import { useRequest } from 'ahooks';
+import { BaseOptions, PaginatedParams } from 'ahooks/lib/useAntdTable';
+import { BaseResult, CombineService } from '@ahooksjs/use-request/lib/types';
+import { useModel } from 'umi';
+
+export default function useModelRequest<T>(
+  fetchData: CombineService<T, PaginatedParams>,
+  options?: BaseOptions<T>,
+): BaseResult<T, PaginatedParams> {
+  const { initialState } = useModel('@@initialState');
+  const { menus = [] } = initialState || {};
+
+  return (useRequest(fetchData, {
+    /** 有菜单权限的数据才会请求 */
+    ready: menus.length > 0,
+    refreshDeps: [menus.length],
+    ...options,
+  }) as unknown) as BaseResult<T, PaginatedParams>;
+}
