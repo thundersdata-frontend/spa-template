2e3b779ea2815fe8fc88b9e930c6169f2907a0d6
chj_damon <chjdamon@gmail.com>
2022-06-08 14:57:47 +0800

    refactor: 清空项目重做

--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { defs as authorizationDefs, authorization } from './authorization';
-
-(window as any).defs = {
-  authorization: authorizationDefs,
-};
-(window as any).API = {
-  authorization,
-};
--- src/layouts/BasicLayout/components/Logo/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import logo from '@/assets/logo.png';
-
-export default function Logo() {
-  return <img src={logo} alt="雷数科技" style={{ width: 32, height: 32 }} />;
-}
